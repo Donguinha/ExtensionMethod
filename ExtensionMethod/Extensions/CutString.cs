bf92b69707f9c1873491084b7005ffbbea36b905
Guilherme Rodrigues <oliveirack1@gmail.com>
2022-12-07 09:54:57 -0300

    first commit

---
+++ ExtensionMethod/Extensions/CutString.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    public static class CutString
+    {
+        public static string StringCut(this string stringObj, int count)
+        {
+            if(stringObj.Length < count)
+            {
+                return stringObj;
+            }
+            else
+            {
+                return stringObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2022, 12, 06, 02,04,10);
+
+            Console.WriteLine(date.ElapsedTime());
+
+            Console.Write("\nPhrase: ");
+            string phrase = Console.ReadLine();
+            Console.Write("Cut: ");
+            int cut = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(phrase.StringCut(cut)); 
+        }
+    }
+}
